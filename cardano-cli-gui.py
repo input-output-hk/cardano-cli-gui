79173374149db1f3a7f6018f17ac49aa80139186
Luka Kurnjek <luka.kurnjek@iohk.io>
2023-02-02 16:07:33 +0100

    Updating main gui file.

--- cardano-cli-gui.py
+++ cardano-cli-gui.py
@@ -8,13 +8,29 @@ from start import Start
 from transactions import Transactions
 from smart_contracts import Smart_contracts
 
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget, QAction)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Cardano client GUI 1.0")
-        
+
+        # Create the applications menu 
+        self.app_menu = self.menuBar()
+        debug_menu = self.app_menu.addMenu("&Debug")
+        help_menu = self.app_menu.addMenu("&Help")
+
+        on_toggle = QAction("&ON", self)
+        off_toggle = QAction("&OFF", self)
+
+        debug_menu.addAction(on_toggle)
+        debug_menu.addSeparator()
+        debug_menu.addAction(off_toggle)
+
+        tutorial_btn = QAction("&Tutorial", self)
+        help_menu.addAction(tutorial_btn)
+
+        # Create tabs and add windows 
         tabs = QTabWidget()
         tabs.setTabPosition(QTabWidget.North)
         tabs.setMovable(True)
